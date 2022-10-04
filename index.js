09daa75a68a18b5fc293bf846f2b6c1331dd2d46
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-10-04 00:45:45 -0500

    corregirdo los conflitos del merge

--- index.js
+++ index.js
@@ -4,34 +4,22 @@ const app = express();
 app.get("/hola", (req, res) => {
   res.send("ruta Hola");
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 app.get("/registro", (req, res) => {
   res.send("ruta de registro");
 });
-=======
->>>>>>> nuevaRutaRegistro
-=======
->>>>>>> nuevaRutaLogin
 
 app.get("/adios", (req, res) => {
   res.send("ruta Adios");
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
 app.get("/registro", (req, res) => {
   res.send("ruta Adios");
 });
->>>>>>> nuevaRutaRegistro
-=======
 
 app.post("/login", (req, res) => {
   res.send("Ruta de login nueva");
 });
->>>>>>> nuevaRutaLogin
 
 app.listen(3000, () => {
   console.log("Server on port 3000");
