9c257742d6641b3d9a2df91f0258e037dd90e586
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-10-04 00:33:24 -0500

    agregar carpeta public y el middleware para los archivos estaticos

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"))
 
 app.get("/hola", (req, res) => {
-  res.send("ruta Hola")
-})
+  res.send("ruta Hola");
+});
+
+app.get("/registro", (req, res) => {
+  res.send("ruta de registro");
+});
 
 app.get("/adios", (req, res) => {
-  res.send("ruta Adios")
-})
+  res.send("ruta Adios");
+});
 
 app.listen(3000, () => {
   console.log("Server on port 3000");
-})
+});
