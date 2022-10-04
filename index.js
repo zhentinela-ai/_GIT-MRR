7ac0ce266eaaa419fdd040941eaf36fff1dcd032
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-10-04 00:08:32 -0500

    ruta adios añadida

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.get("/adios", (req, res) => {
+  res.send("ruta Adios")
+})
+
 app.listen(3000, () => {
   console.log("Server on port 3000");
 })
